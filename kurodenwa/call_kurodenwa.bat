f0b8f030999f3ddbeababb73234f6339d028399b
YugaTukuda <165561853+YugaTukuda@users.noreply.github.com>
2024-04-01 20:33:19 +0900

    Update call_kurodenwa.bat

--- kurodenwa/call_kurodenwa.bat
+++ kurodenwa/call_kurodenwa.bat
@@ -3,5 +3,5 @@
 timeout /t 20 /nobreak
 call C:\Users\kurod\anaconda3\Scripts\activate
 cd /d "C:\Users\kurod\kurodenwa"
-python C:\Users\kurod\kurodenwa\kurodenwa.py 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
+python C:\Users\kurod\kurodenwa\kurodenwa.py sk-########
 goto start
